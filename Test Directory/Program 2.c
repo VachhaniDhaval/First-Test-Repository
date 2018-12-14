12c6a0d55fb5982cf24582c2cae70cf93ad351db
Dhaval Vachhani <45855089+VachhaniDhaval@users.noreply.github.com>
2018-12-14 23:01:17 +0530

    To check modified file commit using Upload files

--- Test Directory/Program 2.c
+++ Test Directory/Program 2.c
@@ -4,4 +4,5 @@ void main
 {
 	printf("Hello World \n");
 	printf("This is Test Directory Program 2 to commit changes using Upload files of GitHub.\n");
+	printf("This Print is added to check modified file commit using Upload files option.\n");
 }
